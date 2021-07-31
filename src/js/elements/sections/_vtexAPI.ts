b0882b45de89ceea273ad395b6273820cc5fa7e1
RaphaelBRodrigues <raphaelbarbosa.rodrigues@gmail.com>
2021-07-31 20:42:22 -0300

    chore: joining common styles

---
+++ src/js/elements/sections/_vtexAPI.ts
@@ -0,0 +1,17 @@
+import CacheSelector from '../__cache-selector';
+
+const { $list, $links } = {
+  ...CacheSelector.storeInfo,
+};
+
+function showEndpoints() {
+
+}
+
+
+
+function init() {
+  showEndpoints();
+}
+
+export default init;
